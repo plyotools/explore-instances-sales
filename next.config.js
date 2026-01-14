6b6c4c71125644c57832097d2826fb99feda3f49
Paul Rene Engejordet <pre@macbookpro.lan>
2026-01-14 19:47:44 +0100

    Regenerate with correct basePath and fixed panorama-viewer component

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   // Only use static export in production builds
   ...(process.env.NODE_ENV === 'production' && { output: 'export' }),
-  basePath: process.env.NODE_ENV === 'production' ? '/explore' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/explore' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/explore-instances-sales' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/explore-instances-sales' : '',
   images: {
     unoptimized: true,
   },
